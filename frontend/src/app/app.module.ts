e5f5a19a5d836bbac94f08b31cecce4680de0456
Tasmia Zerin <ttaosnmniya.6638.cross@gmail.com>
2023-02-14 02:02:11 +0600

    adding sign-in

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './modules/landing-page/landing-page.component';
+import { SignInComponent } from './modules/auth/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
