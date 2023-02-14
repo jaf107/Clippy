9f65287db1b345460ad7df85e8af34c3b2b8c792
Tasmia Zerin <ttaosnmniya.6638.cross@gmail.com>
2023-02-15 01:19:41 +0600

    sign in UI complete

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './modules/auth/sign-in/sign-in.component';
+import { LandingPageComponent } from './modules/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'login', component: SignInComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
