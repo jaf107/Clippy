381bbada8c469742f8f3c01419487dd7fe8f037a
Tasmia Zerin <ttaosnmniya.6638.cross@gmail.com>
2023-02-25 23:29:29 +0600

    doing summarizer

--- frontend/src/app/modules/summarizer/summarizer.component.ts
+++ frontend/src/app/modules/summarizer/summarizer.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as extractiveSummary from '../../../assets/preprocessed.json';
+
+
+interface chunk {
+  "title": String,
+  "noOfSentences": Number,
+  "text": String,
+  "summaryText": String
+}
 
 @Component({
   selector: 'app-summarizer',
   templateUrl: './summarizer.component.html',
   styleUrls: ['./summarizer.component.css']
 })
-export class SummarizerComponent {
+export class SummarizerComponent implements OnInit{
 
+  public summary : any = (extractiveSummary as any).default;
+
+  ngOnInit(){
+    // this.summary = extractiveSummary;
+    console.log(extractiveSummary);
+    
+    // for(let i=0; i<this.summary.length; i++){
+    //   console.log(this.summary);
+    // }
+  }
 }
