48bd77fe696487bf5b534c8eae22b90ff1483901
jsureka <bsse1115@iit.du.ac.bd>
2023-02-26 16:16:47 +0600

    paper controller updated

--- server/app/controllers/paper.controller.js
+++ server/app/controllers/paper.controller.js
@@ -4,6 +4,7 @@ const Paper = db.paper;
 const cloudinary = require("cloudinary");
 const axios = require("axios");
 const SEMANTIC_SCHOLAR_API = "https://api.semanticscholar.org/graph/v1/paper/";
+
 exports.uploadPaper = async (req, res) => {
   const result = await cloudinary.v2.uploader
     .upload(req.params.id, {
@@ -23,33 +24,38 @@ exports.uploadPaper = async (req, res) => {
 };
 
 exports.uploadPaperById = async (req, res) => {
-  const paper_data = await axios
-    .get(
-      SEMANTIC_SCHOLAR_API +
-        req.body.paper_id +
-        "?fields=isOpenAccess,openAccessPdf"
-    )
-    .catch((err) => res.status(404).send("Paper Not Found"));
+  const ppr = await Paper.find((e) => e.paper_id === req.body.paper_id);
+  if (ppr) {
+    res.status(200).send(paper);
+  } else {
+    const paper_data = await axios
+      .get(
+        SEMANTIC_SCHOLAR_API +
+          req.body.paper_id +
+          "?fields=isOpenAccess,openAccessPdf"
+      )
+      .catch((err) => res.status(404).send("Paper Not Found"));
 
-  if (paper_data.data) {
-    if (!paper_data.data.isOpenAccess || !paper_data.data.openAccessPdf)
-      res.status(404).send("Paper Not Accessible");
-    else {
-      const result = await cloudinary.v2.uploader
-        .upload(paper_data.data.openAccessPdf.url, {
-          folder: "papers",
-        })
-        .catch((err) => res.status(500).send(err));
+    if (paper_data.data) {
+      if (!paper_data.data.isOpenAccess || !paper_data.data.openAccessPdf)
+        res.status(404).send("Paper Not Accessible");
+      else {
+        const result = await cloudinary.v2.uploader
+          .upload(paper_data.data.openAccessPdf.url, {
+            folder: "papers",
+          })
+          .catch((err) => res.status(500).send(err));
 
-      if (result) {
-        var paper = {
-          // user_id: req.userId,
-          paper_id: req.body.paper_id,
-          public_id: result.public_id,
-          url: result.secure_url,
-        };
-        await Paper.create(paper);
-        res.status(200).send(paper);
+        if (result) {
+          var paper = {
+            // user_id: req.userId,
+            paper_id: req.body.paper_id,
+            public_id: result.public_id,
+            url: result.secure_url,
+          };
+          await Paper.create(paper);
+          res.status(200).send(paper);
+        }
       }
     }
   }
--- server/app/routes/paper.routes.js
+++ server/app/routes/paper.routes.js
@@ -6,7 +6,7 @@ const router = new express.Router();
 
 // router.route("/all").get(controller.allAccess);
 // router.route("").get([authJwt.verifyToken], controller.userBoard);
-router.route("/uploadById").post(controller.uploadPaperById);
+router.route("/uploadById").get(controller.uploadPaperById);
 // router.route("/upload").post(controller.uploadPaperByUrl);
 
 module.exports = router;
