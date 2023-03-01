f3d17ba461eee10f5a63663748d4b40b29f68038
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-03-02 04:32:36 +0600

    unfinished wrapper

--- pdfjs-test/newTest.js
+++ pdfjs-test/newTest.js
@@ -1,19 +1,11 @@
 const pdfjs = require("pdfjs-dist/legacy/build/pdf.js");
-const pdfLinkService = require("pdfjs-dist/lib/web/pdf_link_service");
 const fs = require("fs");
 
-/**
- * Extarct texts from a pdf file
- * @param {string} src
- */
 async function getPdfTextContent(src) {
   const doc = await pdfjs.getDocument(src).promise;
   const totalPageCount = doc.numPages;
   doc.getDestination;
   // clear the text file //
-  fs.truncateSync("./output.txt", 0);
-  fs.truncateSync("./object.txt", 0);
-
   let textChunkArray = [];
   heights = [];
 
@@ -21,33 +13,14 @@ async function getPdfTextContent(src) {
     const page = await doc.getPage(i);
     const textContent = await page.getTextContent();
     textChunkArray.push(getTextChunkObject(textContent));
-    writeToFile("./output.txt", textContent, i);
-    writeObjectToFile("./object.txt", textContent, i);
-    writeUniqueHeights(heights, textContent);
+    getUniqueHeights(heights, textContent);
   }
   const uniqueHeight = new Set(heights);
   heights = Array.from(uniqueHeight);
-  sortedHeights = heights.sort(function (a, b) {
-    return b - a;
-  });
-
-  // sortedHeights.forEach((element) => {
-  //   fs.appendFileSync(
-  //     "./heights.txt",
-  //     JSON.stringify(element) + "\n",
-  //     (err) => {
-  //       if (err) {
-  //         throw err;
-  //       }
-  //     }
-  //   );
-  // });
-
-  // fs.appendFileSync("./heights.txt", JSON.stringify(height));
   return { textChunkArray, uniqueHeight };
 }
 
-function writeUniqueHeights(heights, content) {
+function getUniqueHeights(heights, content) {
   const items = content.items.map((item) => {
     heights.push(item.height);
   });
@@ -63,58 +36,9 @@ function getTextChunkObject(content) {
   return retItems;
 }
 
-function writeObjectToFile(filePath, content, pageNo) {
-  let data = `page No: ${pageNo} \n===============\n`;
-  let pos = 0;
-
-  const items = content.items.map((item) => {
-    let style = content.styles[item.fontName];
-
-    heights.push(item.height);
-
-    data += `item no ${pos}
-    ------------------------
-    item.str=${item.str}
-    item.dir=${item.dir}
-    item.width=${item.width}
-    item.height=${item.height}
-    item.transforme=${item.transform}
-    item.hasEOL=${item.hasEOL}
-    item.fontName=${style.fontFamily}
-    
-    content.styles object for this chunk
-    ${JSON.stringify(style)}
-    --------------------------------\n`;
-    pos++;
-  });
-
-  data += `\n`;
-
-  // fs.appendFileSync("./heights.txt", uniqueHeight);
-  fs.appendFileSync(filePath, data);
-}
-
-function writeToFile(filePath, content, pageNo) {
-  let data = `page No: ${pageNo} \n===============\n`;
-  const items = content.items.map((item) => {
-    data += item.str + `\n`;
-  });
-  data += `\n`;
-  fs.appendFileSync(filePath, data);
-}
-
-function writeChunksArrayToFile(filepath, chunksArray) {
-  fs.truncateSync(filepath, 0);
-  fs.appendFileSync(filepath, JSON.stringify(chunksArray), (err) => {
-    if (err) {
-      throw err;
-    }
-  });
-}
-
-async function wrapper() {
+async function wrapper(src) {
   let { textChunkArray: arr, uniqueHeight: heights } = await getPdfTextContent(
-    "./sample4.pdf"
+    src
   );
   let freq = Array.from(heights).map((item) => {
     return { height: item, count: 0 };
@@ -216,9 +140,10 @@ async function wrapper() {
       }
     });
   }
-  writeChunksArrayToFile("./finalOut.json", finalOut);
+  console.log(finalOut);
+  return finalOut;
 }
-wrapper();
+wrapper("./sample9.pdf");
 
 function extractKeywordFromStr(chunkStr, type) {
   chunkStr = chunkStr.toLowerCase();
@@ -229,47 +154,6 @@ function extractKeywordFromStr(chunkStr, type) {
   textStr = splittedArr[0] + " " + splittedArr[1];
   return textStr;
 }
-
-async function test() {
-  const { textChunkArray: pdfChunks, uniqueHeight: heights } =
-    await getPdfTextContent("./sample4.pdf");
-  // console.log("dhukse", pdfChunks);
-
-  function getPageHeight(pdfChunks) {
-    let max = 0;
-    pdfChunks.map((pageChunks, pageIndex) => {
-      let lastChunkIndex = pageChunks.length - 1;
-      console.log(
-        "page NO:",
-        pageIndex + 1,
-        "Chunks in a page:",
-        pageChunks.length,
-        "starting chunk height:",
-        pageChunks[0].height,
-        "starting chunk y:",
-        pageChunks[0].transform[5],
-        "last chunk y:",
-        pageChunks[lastChunkIndex].transform[5],
-        "y diffrence:",
-        pageChunks[0].transform[5] - pageChunks[lastChunkIndex].transform[5]
-      );
-    });
-  }
-
-  function getMaxChunkDistanceInAPage(pageChunks) {
-    let maxDis = 0;
-    for (let i = 1; i < pageChunks.length; i++) {
-      let dis = Math.abs(
-        pageChunks[i].transform[5] - pageChunks[i - 1].transform[5]
-      );
-      if (dis > maxDis) maxDis = dis;
-    }
-    return maxDis;
-  }
-
-  getPageHeight(pdfChunks);
-}
-// test();
 
 function isAlphanumeric(str) {
   return /^[a-zA-Z0-9 =_\/+\-:.,;'"“”<>#@?!%&()*{}\[\]$]+$/.test(str);
---
+++ pdfjs-test/pdf2json/esmToCommonWrapper/wrapper.js
@@ -0,0 +1,6 @@
+const PDFParser = import("pdf2json");
+module.exports = { PDFParser };
+
+// console.log(PDFParser);
+
+// module.exports = PDFParser;
