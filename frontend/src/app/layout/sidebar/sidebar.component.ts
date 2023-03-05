c65825f9873a02d4d45eab2089778f8708b5f625
musta55 <bsse1114@iit.du.ac.bd>
2023-03-05 16:37:43 +0600

    change referenceCount

--- frontend/src/app/layout/sidebar/sidebar.component.ts
+++ frontend/src/app/layout/sidebar/sidebar.component.ts
@@ -37,7 +37,9 @@ export class SidebarComponent {
   ngOnInit(): void {
 
     this.pdfData = JSON.parse(this.tokenStorage.getPaperData());
-    console.log(this.pdfData);
+ //   console.log(this.pdfData);
+ {{ console.log('pdfData:', this.pdfData) }}
+ {{ console.log('citationCount:', this.pdfData?.citationCount) }}
 
     
     this.featureService.getAbsSummarizerStatus().subscribe((value) => {
